c4f8920eaf87f71c6b589273456422687bc8b84f
Yurii Timofeev <georgius@ural.ru>
2021-07-05 00:07:09 +0500

    renamed expension file

--- src/App.vue
+++ src/App.vue
@@ -17,149 +17,7 @@
 import "medium-editor/dist/css/medium-editor.css";
 import "medium-editor/dist/css/themes/flat.css";
 import editor from "vue2-medium-editor";
-
-const MediumEditor = editor.MediumEditor;
-var TestExtension = editor.MediumEditor.Extension.extend({
-  name: "test-ext",
-
-  init() {
-    this.button = this.document.createElement("button");
-    this.button.classList.add("medium-editor-action");
-    this.button.innerHTML = `
-      <div class="size-picker-widget">
-        <input class="display">
-        <div class="controls">
-          <div class="inc icon">
-            <svg style="width:12px;height:12px" viewBox="0 0 24 24">
-              <path fill="currentColor" d="M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z" />
-            </svg>
-          </div>
-          <div class="dec icon">
-            <svg style="width:12px;height:12px" viewBox="0 0 24 24">
-              <path fill="currentColor" d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-            </svg>
-          </div>
-        </div>
-      </div>`;
-    this.currentSize = "";
-    this.on(
-      this.button.querySelector(".inc"),
-      "click",
-      this.handleInc.bind(this)
-    );
-    this.on(
-      this.button.querySelector(".dec"),
-      "click",
-      this.handleDec.bind(this)
-    );
-    this.on(this.base.origElements, "click", this.updateCurrentSize.bind(this));
-    this.on(
-      this.button.querySelector(".display"),
-      "keydown",
-      this.handleKeyEnter.bind(this)
-    );
-    this.base.subscribe("positionToolbar", this.saveSelection.bind(this));
-  },
-  getButton() {
-    return this.button;
-  },
-  updateCurrentSize() {
-    const range = MediumEditor.selection.getSelectionRange(this.document);
-    this.currentSize = window.getComputedStyle(
-      range.startContainer.parentElement
-    ).fontSize;
-    this.displayCurrentSize();
-  },
-  displayCurrentSize() {
-    this.button.querySelector(".display").value = this.currentSize;
-  },
-  saveSelection() {
-    this.button.querySelector(".display").selection =
-      this.base.exportSelection();
-  },
-  handleInc() {
-    this.currentSize = `${+this.currentSize.slice(0, -2) + 1}px`;
-    this.applyCurrentSize();
-  },
-  handleDec() {
-    this.currentSize = `${+this.currentSize.slice(0, -2) - 1}px`;
-    this.applyCurrentSize();
-  },
-  handleKeyEnter(event) {
-    const savedSelection = this.button.querySelector(".display").selection;
-    console.log(savedSelection);
-    if (!savedSelection) {
-      return;
-    }
-    const fontSizeAction = (callback) => {
-      event.preventDefault();
-      event.stopPropagation();
-      this.base.importSelection(savedSelection, true);
-      this.currentSize = event.target.value;
-      callback();
-      event.target.focus();
-    };
-    switch (event.key) {
-      case "Enter":
-        fontSizeAction(() => {
-          this.currentSize = event.target.value;
-          this.applyCurrentSize();
-        });
-        break;
-      case "ArrowUp":
-        fontSizeAction(() => {
-          this.currentSize = `${+this.currentSize.slice(0, -2) + 1}px`;
-          this.applyCurrentSize();
-        });
-        break;
-      case "ArrowDown":
-        fontSizeAction(() => {
-          this.currentSize = `${+this.currentSize.slice(0, -2) - 1}px`;
-          this.applyCurrentSize();
-        });
-        break;
-    }
-  },
-
-  applyCurrentSize() {
-    let selectionState = this.base.exportSelection();
-    if (selectionState.start === selectionState.end) {
-      const range = MediumEditor.selection.getSelectionRange(this.document);
-      this.base.selectElement(range.startContainer.parentElement);
-      const start = range.startContainer.parentElement;
-      if (start.nodeType === 3 || start.nodeName === "BR") {
-        this.base.selectElement(start.parentElement);
-      } else {
-        this.base.selectElement(start);
-      }
-      selectionState = this.base.exportSelection();
-    }
-    const fontName = "imaginary";
-    this.document.execCommand("fontName", false, fontName);
-    const fontElement = this.document.querySelector('font[face="imaginary"]');
-    let parent = fontElement;
-
-    while (
-      parent.innerText === parent.parentElement.innerText &&
-      parent.parentElement !== this.base.origElements
-    ) {
-      parent = parent.parentElement;
-    }
-
-    if (fontElement === parent) {
-      const spanElement = this.document.createElement("span");
-      spanElement.innerText = fontElement.innerText;
-      spanElement.style.fontSize = this.currentSize;
-      parent.parentNode.replaceChild(spanElement, fontElement);
-    } else {
-      this.document.execCommand("undo", false);
-      parent.style.fontSize = this.currentSize;
-    }
-    this.base.importSelection(selectionState, true);
-    this.displayCurrentSize();
-    this.base.checkContentChanged();
-  },
-});
+import FontSizeButton from './FontSizeButton'
 
 export default {
   name: "App",
@@ -171,10 +29,10 @@ export default {
       text: `<p style="font-size: 36px;">Click <span style="font-size: 27px;">here</span>&nbsp; to edit it or <span style="color: rgb(240, 50, 230);">highlight</span> the text to style it</p>`,
       options: {
         extensions: {
-          "test-ext": new TestExtension(),
+          "font-size": new FontSizeButton(),
         },
         toolbar: {
-          buttons: ["test-ext", "bold"],
+          buttons: ["font-size", "bold"],
           static: true,
           sticky: true,
           align: "center",
---
+++ src/FontSizeButton.js
@@ -0,0 +1,147 @@
+import editor from "vue2-medium-editor";
+
+const MediumEditor = editor.MediumEditor;
+
+const FontSizeButton = editor.MediumEditor.Extension.extend({
+  name: "font-size",
+
+  init() {
+    this.button = this.document.createElement("button");
+    this.button.classList.add("medium-editor-action");
+    this.button.innerHTML = `
+      <div class="size-picker-widget">
+        <input class="display">
+        <div class="controls">
+          <div class="inc icon">
+            <svg style="width:12px;height:12px" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z" />
+            </svg>
+          </div>
+          <div class="dec icon">
+            <svg style="width:12px;height:12px" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+            </svg>
+          </div>
+        </div>
+      </div>`;
+    this.currentSize = "";
+    this.on(
+      this.button.querySelector(".inc"),
+      "click",
+      this.handleInc.bind(this)
+    );
+    this.on(
+      this.button.querySelector(".dec"),
+      "click",
+      this.handleDec.bind(this)
+    );
+    this.on(this.base.origElements, "click", this.updateCurrentSize.bind(this));
+    this.on(
+      this.button.querySelector(".display"),
+      "keydown",
+      this.handleKeyEnter.bind(this)
+    );
+    this.base.subscribe("positionToolbar", this.saveSelection.bind(this));
+  },
+  getButton() {
+    return this.button;
+  },
+  updateCurrentSize() {
+    const range = MediumEditor.selection.getSelectionRange(this.document);
+    this.currentSize = window.getComputedStyle(
+      range.startContainer.parentElement
+    ).fontSize;
+    this.displayCurrentSize();
+  },
+  displayCurrentSize() {
+    this.button.querySelector(".display").value = this.currentSize;
+  },
+  saveSelection() {
+    this.button.querySelector(".display").selection =
+      this.base.exportSelection();
+  },
+  handleInc() {
+    this.currentSize = `${+this.currentSize.slice(0, -2) + 1}px`;
+    this.applyCurrentSize();
+  },
+  handleDec() {
+    this.currentSize = `${+this.currentSize.slice(0, -2) - 1}px`;
+    this.applyCurrentSize();
+  },
+  handleKeyEnter(event) {
+    const savedSelection = this.button.querySelector(".display").selection;
+    console.log(savedSelection);
+    if (!savedSelection) {
+      return;
+    }
+    const fontSizeAction = (callback) => {
+      event.preventDefault();
+      event.stopPropagation();
+      this.base.importSelection(savedSelection, true);
+      this.currentSize = event.target.value;
+      callback();
+      event.target.focus();
+    };
+    switch (event.key) {
+      case "Enter":
+        fontSizeAction(() => {
+          this.currentSize = event.target.value;
+          this.applyCurrentSize();
+        });
+        break;
+      case "ArrowUp":
+        fontSizeAction(() => {
+          this.currentSize = `${+this.currentSize.slice(0, -2) + 1}px`;
+          this.applyCurrentSize();
+        });
+        break;
+      case "ArrowDown":
+        fontSizeAction(() => {
+          this.currentSize = `${+this.currentSize.slice(0, -2) - 1}px`;
+          this.applyCurrentSize();
+        });
+        break;
+    }
+  },
+
+  applyCurrentSize() {
+    let selectionState = this.base.exportSelection();
+    if (selectionState.start === selectionState.end) {
+      const range = MediumEditor.selection.getSelectionRange(this.document);
+      this.base.selectElement(range.startContainer.parentElement);
+      const start = range.startContainer.parentElement;
+      if (start.nodeType === 3 || start.nodeName === "BR") {
+        this.base.selectElement(start.parentElement);
+      } else {
+        this.base.selectElement(start);
+      }
+      selectionState = this.base.exportSelection();
+    }
+    const fontName = "imaginary";
+    this.document.execCommand("fontName", false, fontName);
+    const fontElement = this.document.querySelector('font[face="imaginary"]');
+    let parent = fontElement;
+
+    while (
+      parent.innerText === parent.parentElement.innerText &&
+      parent.parentElement !== this.base.origElements
+    ) {
+      parent = parent.parentElement;
+    }
+
+    if (fontElement === parent) {
+      const spanElement = this.document.createElement("span");
+      spanElement.innerText = fontElement.innerText;
+      spanElement.style.fontSize = this.currentSize;
+      parent.parentNode.replaceChild(spanElement, fontElement);
+    } else {
+      this.document.execCommand("undo", false);
+      parent.style.fontSize = this.currentSize;
+    }
+    this.base.importSelection(selectionState, true);
+    this.displayCurrentSize();
+    this.base.checkContentChanged();
+  },
+});
+
+export default FontSizeButton;
