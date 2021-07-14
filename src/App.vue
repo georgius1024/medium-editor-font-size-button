63e1b5a60f0534c5ba5161139abb31b48e4cd315
Timofeev Yurii <georgius@ural.ru>
2021-07-14 23:07:13 +0500

    introduced editor component

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 <template>
   <div id="app">
+    <h1>Title</h1>
     <div style="display: flex">
       <div style="border-right: 1px solid #333; width: 500px">
-        <h1>Title</h1>
-        <div ref="title" v-html="title" v-once />
+        <Editor v-model="title" @input="title = fixLinks($event)" />
       </div>
       <div style="width: 400px">
         <code>{{ title }}</code>
       </div>
     </div>
     <hr />
+    <h1>Description</h1>
     <div style="display: flex">
       <div style="border-right: 1px solid #333; width: 500px">
-        <h1>Description</h1>
-        <div ref="description" v-html="description" v-once />
+        <Editor v-model="description" @input="description = fixLinks($event)" />
       </div>
       <div style="width: 400px">
         <code>{{ description }}</code>
       </div>
     </div>
     <hr />
+    <h1>Button</h1>
     <div style="display: flex">
       <div style="border-right: 1px solid #333; width: 500px">
-        <h1>Button</h1>
-        <div ref="button" v-html="button" v-once />
+        <Editor v-model="button" @input="button = fixLinks($event)" />
       </div>
       <div style="width: 400px">
         <code>{{ button }}</code>
@@ -34,20 +34,19 @@
       Link color
       <input v-model="linkColor" type="color" @input="paintLinks" />
     </label>
-    <br />
-    <button @click="reset">Reset</button>
   </div>
 </template>
 
 <script>
-import MediumEditor from "medium-editor";
+// import MediumEditor from "medium-editor";
 
-import "medium-editor/dist/css/medium-editor.css";
-import "medium-editor/dist/css/themes/mani.css";
-import FontSizeButton from "./FontSizeButton";
-import FontNameButton from "./FontNameButton";
-import LineHeightButton from "./LineHeightButton";
-import LinkForm from "./LinkForm";
+// import "medium-editor/dist/css/medium-editor.css";
+// import "medium-editor/dist/css/themes/mani.css";
+// import FontSizeButton from "./FontSizeButton";
+// import FontNameButton from "./FontNameButton";
+// import LineHeightButton from "./LineHeightButton";
+// import LinkForm from "./LinkForm";
+import Editor from "./Editor";
 
 const getTextWithFixedLinksColor = (text, color) => {
   const doc = new DOMParser().parseFromString(text, "text/html");
@@ -60,6 +59,9 @@ const getTextWithFixedLinksColor = (text, color) => {
 
 export default {
   name: "App",
+  components: {
+    Editor,
+  },
   data() {
     return {
       linkColor: "#00ff00",
@@ -70,127 +72,119 @@ export default {
       <p>It comes as no surprise the Gel-Lyte III is a fast growing popular choice for sneaker enthusiasts all over the world.</p>
       `,
       button: `<a href="#" class="button">Shop $ 242</a>`,
-      displaybutton: "",
-      displayDescription: "",
-      displayButton: "",
-      key: 1,
+      cs: `Visual Studio Code is a lightweight but powerful source code editor which runs on your desktop and is available for Windows, macOS and Linux. It comes with built-in support for JavaScript, TypeScript and Node.js and has a rich ecosystem of extensions for other languages (such as C++, C#, Java, Python, PHP, Go) and runtimes (such as .NET and Unity). Begin your journey with VS Code with these introductory videos.`,
     };
   },
   computed: {},
   created() {
     this.paintLinks();
   },
-  mounted() {
-    this.titleEditor = new MediumEditor(this.$refs.title, this.options());
-    this.titleInput = () =>
-      this.updateTitle(this.titleEditor.origElements.innerHTML);
-    this.titleEditor.subscribe("editableInput", this.titleInput);
-    this.descriptionEditor = new MediumEditor(
-      this.$refs.description,
-      this.options()
-    );
-    this.descriptionInput = () =>
-      this.updateDescription(this.descriptionEditor.origElements.innerHTML);
-    this.descriptionEditor.subscribe("editableInput", this.descriptionInput);
-    this.buttonEditor = new MediumEditor(this.$refs.button, this.options());
-    this.buttonInput = () =>
-      (this.button = getTextWithFixedLinksColor(
-        this.buttonEditor.origElements.innerHTML,
-        this.linkColor
-      ));
-    this.buttonEditor.subscribe("editableInput", this.buttonInput);
-  },
-  beforeDestroy() {
-    this.titleEditor.unsubscribe("editableInput", this.titleInput);
-    this.descriptionEditor.unsubscribe("editableInput", this.descriptionInput);
-    this.buttonEditor.unsubscribe("editableInput", this.buttonInput);
-  },
+  // mounted() {
+  //   this.titleEditor = new MediumEditor(this.$refs.title, this.options());
+  //   this.titleInput = () =>
+  //     this.updateTitle(this.titleEditor.origElements.innerHTML);
+  //   this.titleEditor.subscribe("editableInput", this.titleInput);
+  //   this.descriptionEditor = new MediumEditor(
+  //     this.$refs.description,
+  //     this.options()
+  //   );
+  //   this.descriptionInput = () =>
+  //     this.updateDescription(this.descriptionEditor.origElements.innerHTML);
+  //   this.descriptionEditor.subscribe("editableInput", this.descriptionInput);
+  //   this.buttonEditor = new MediumEditor(this.$refs.button, this.options());
+  //   this.buttonInput = () =>
+  //     (this.button = getTextWithFixedLinksColor(
+  //       this.buttonEditor.origElements.innerHTML,
+  //       this.linkColor
+  //     ));
+  //   this.buttonEditor.subscribe("editableInput", this.buttonInput);
+  // },
+  // beforeDestroy() {
+  //   this.titleEditor.unsubscribe("editableInput", this.titleInput);
+  //   this.descriptionEditor.unsubscribe("editableInput", this.descriptionInput);
+  //   this.buttonEditor.unsubscribe("editableInput", this.buttonInput);
+  // },
   methods: {
-    options() {
-      return {
-        extensions: {
-          "font-size": new FontSizeButton(),
-          "font-name": new FontNameButton(),
-          "line-height": new LineHeightButton(),
-          "link-form": new LinkForm(() => this.paintLinks()),
-        },
-        toolbar: {
-          buttons: [
-            "font-name",
-            "font-size",
-            "line-height",
-            "bold",
-            "italic",
-            "underline",
-            "anchor",
-            "unorderedlist",
-            "justifyLeft",
-            "justifyCenter",
-            "justifyRight",
-            "removeFormat",
-          ],
-          static: true,
-          sticky1: true,
-          align: "center",
-          positionStaticToolbar(container) {
-            const scrollTop =
-              (this.document.documentElement &&
-                this.document.documentElement.scrollTop) ||
-              this.document.body.scrollTop;
-            const toolbarElement = this.getToolbarElement();
-            const containerRect = container.getBoundingClientRect();
-            const toolbarHeight = toolbarElement.offsetHeight;
-            toolbarElement.style.top = `${
-              containerRect.top + scrollTop - toolbarHeight - 20
-            }px`;
-          },
-          updateOnEmptySelection: true,
-        },
-        placeholder: {
-          text: "Type your text ПРЯМО ТУТ!!!",
-          hideOnClick: true,
-        },
-      };
+    // options() {
+    //   return {
+    //     extensions: {
+    //       "font-size": new FontSizeButton(),
+    //       "font-name": new FontNameButton(),
+    //       "line-height": new LineHeightButton(),
+    //       "link-form": new LinkForm(() => this.paintLinks()),
+    //     },
+    //     toolbar: {
+    //       buttons: [
+    //         "font-name",
+    //         "font-size",
+    //         "line-height",
+    //         "bold",
+    //         "italic",
+    //         "underline",
+    //         "anchor",
+    //         "unorderedlist",
+    //         "justifyLeft",
+    //         "justifyCenter",
+    //         "justifyRight",
+    //         "removeFormat",
+    //       ],
+    //       static: true,
+    //       sticky1: true,
+    //       align: "center",
+    //       positionStaticToolbar(container) {
+    //         const scrollTop =
+    //           (this.document.documentElement &&
+    //             this.document.documentElement.scrollTop) ||
+    //           this.document.body.scrollTop;
+    //         const toolbarElement = this.getToolbarElement();
+    //         const containerRect = container.getBoundingClientRect();
+    //         const toolbarHeight = toolbarElement.offsetHeight;
+    //         toolbarElement.style.top = `${
+    //           containerRect.top + scrollTop - toolbarHeight - 20
+    //         }px`;
+    //       },
+    //       updateOnEmptySelection: true,
+    //     },
+    //     placeholder: {
+    //       text: "Type your text ПРЯМО ТУТ!!!",
+    //       hideOnClick: true,
+    //     },
+    //   };
+    // },
+    fixLinks(value) {
+      return getTextWithFixedLinksColor(value, this.linkColor);
     },
-    updateTitle(value) {
-      const fixed = getTextWithFixedLinksColor(value, this.linkColor);
-      if (fixed !== value) {
-        const selection = this.titleEditor.exportSelection();
-        this.titleEditor.setContent(fixed, 0);
-        this.$refs.title.innerHTML = fixed;
-        this.titleEditor.importSelection(selection);
-      }
-      this.title = fixed;
-    },
-    updateDescription(value) {
-      const fixed = getTextWithFixedLinksColor(value, this.linkColor);
-      if (fixed !== value) {
-        const selection = this.descriptionEditor.exportSelection();
-        this.descriptionEditor.setContent(fixed, 0);
-        this.$refs.description.innerHTML = fixed;
-        this.descriptionEditor.importSelection(selection);
-      }
-      this.description = fixed;
-    },
-    updateButton(operation) {
-      this.button = getTextWithFixedLinksColor(
-        operation.api.origElements.innerHTML,
-        this.linkColor
-      );
-    },
-    reset() {
-      this.button = this.$refs.button.$el.innerHTML;
-      this.description = this.$refs.description.$el.innerHTML;
-      this.button = this.$refs.button.$el.innerHTML;
-      this.key = this.key + 1;
-    },
+    // updateTitle(value) {
+    //   const fixed = getTextWithFixedLinksColor(value, this.linkColor);
+    //   if (fixed !== value) {
+    //     const selection = this.titleEditor.exportSelection();
+    //     this.titleEditor.setContent(fixed, 0);
+    //     this.$refs.title.innerHTML = fixed;
+    //     this.titleEditor.importSelection(selection);
+    //   }
+    //   this.title = fixed;
+    // },
+    // updateDescription(value) {
+    //   const fixed = getTextWithFixedLinksColor(value, this.linkColor);
+    //   if (fixed !== value) {
+    //     const selection = this.descriptionEditor.exportSelection();
+    //     this.descriptionEditor.setContent(fixed, 0);
+    //     this.$refs.description.innerHTML = fixed;
+    //     this.descriptionEditor.importSelection(selection);
+    //   }
+    //   this.description = fixed;
+    // },
+    // updateButton(operation) {
+    //   this.button = getTextWithFixedLinksColor(
+    //     operation.api.origElements.innerHTML,
+    //     this.linkColor
+    //   );
+    // },
     paintLinks() {
-      this.button = getTextWithFixedLinksColor(this.button, this.linkColor);
-      this.description = getTextWithFixedLinksColor(
-        this.description,
-        this.linkColor
-      );
-      this.button = getTextWithFixedLinksColor(this.button, this.linkColor);
+      this.cs = this.fixLinks(this.cs);
+      this.title = this.fixLinks(this.title);
+      this.description = this.fixLinks(this.description);
+      this.button = this.fixLinks(this.button);
     },
   },
 };
---
+++ src/Editor.vue
@@ -0,0 +1,158 @@
+<template>
+  <div>
+    <div ref="editor" v-once class="editor" v-html="value" />
+  </div>
+</template>
+<script>
+import MediumEditor from "medium-editor";
+import "medium-editor/dist/css/medium-editor.css";
+import "./Editor.css";
+import FontSizeButton from "./FontSizeButton";
+import FontNameButton from "./FontNameButton";
+import LineHeightButton from "./LineHeightButton";
+import LinkForm from "./LinkForm";
+
+export default {
+  name: "Editor",
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+    options: {
+      type: Object,
+      default() {
+        return {
+          extensions: {
+            "font-size": new FontSizeButton(),
+            "font-name": new FontNameButton(),
+            "line-height": new LineHeightButton(),
+            "link-form": new LinkForm(),
+          },
+          toolbar: {
+            buttons: [
+              "font-name",
+              "font-size",
+              "line-height",
+              "bold",
+              "italic",
+              "underline",
+              "anchor",
+              "unorderedlist",
+              "justifyLeft",
+              "justifyCenter",
+              "justifyRight",
+              "removeFormat",
+            ],
+            static: true,
+            sticky1: true,
+            align: "center",
+            positionStaticToolbar(container) {
+              const scrollTop =
+                (this.document.documentElement &&
+                  this.document.documentElement.scrollTop) ||
+                this.document.body.scrollTop;
+              const toolbarElement = this.getToolbarElement();
+              const containerRect = container.getBoundingClientRect();
+              const toolbarHeight = toolbarElement.offsetHeight;
+              toolbarElement.style.top = `${Math.max(
+                0,
+                containerRect.top + scrollTop - toolbarHeight - 20
+              )}px`;
+            },
+            updateOnEmptySelection: true,
+          },
+          placeholder: {
+            text: "Type your text ПРЯМО ТУТ!!!",
+            hideOnClick: true,
+          },
+        };
+      },
+      required: false,
+    },
+  },
+  data() {
+    return {
+      editor: null,
+    };
+  },
+
+  computed: {
+    options1() {
+      return {
+        extensions: {
+          "font-size": new FontSizeButton(),
+          "font-name": new FontNameButton(),
+          "line-height": new LineHeightButton(),
+          "link-form": new LinkForm(),
+        },
+        toolbar: {
+          buttons: [
+            "font-name",
+            "font-size",
+            "line-height",
+            "bold",
+            "italic",
+            "underline",
+            "anchor",
+            "unorderedlist",
+            "justifyLeft",
+            "justifyCenter",
+            "justifyRight",
+            "removeFormat",
+          ],
+          static: true,
+          sticky1: true,
+          align: "center",
+          positionStaticToolbar(container) {
+            const scrollTop =
+              (this.document.documentElement &&
+                this.document.documentElement.scrollTop) ||
+              this.document.body.scrollTop;
+            const toolbarElement = this.getToolbarElement();
+            const containerRect = container.getBoundingClientRect();
+            const toolbarHeight = toolbarElement.offsetHeight;
+            toolbarElement.style.top = `${
+              containerRect.top + scrollTop - toolbarHeight - 20
+            }px`;
+          },
+          updateOnEmptySelection: true,
+        },
+        placeholder: {
+          text: "Type your text ПРЯМО ТУТ!!!",
+          hideOnClick: true,
+        },
+      };
+    },
+  },
+  watch: {
+    value(newValue, oldValue) {
+      if (oldValue !== newValue) {
+        const selection = this.editor.exportSelection();
+        this.editor.setContent(newValue);
+        this.editor.importSelection(selection);
+      }
+    },
+  },
+  created() {
+    this.inputEvent = () => this.$emit("input", this.$refs.editor.innerHTML);
+  },
+  mounted() {
+    this.editor = new MediumEditor(this.$refs.editor, this.options);
+    this.editor.subscribe("editableInput", this.inputEvent);
+  },
+  beforeDestroy() {
+    this.editor.unsubscribe("editableInput", this.inputEvent);
+  },
+  methods: {
+    updated(value) {
+      this.$emit("input", value);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.editor {
+  display: block;
+}
+</style>
